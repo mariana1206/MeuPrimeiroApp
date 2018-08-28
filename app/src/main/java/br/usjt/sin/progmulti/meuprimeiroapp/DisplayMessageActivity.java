a5c0739b00189b193dde0567a3bedbbcfe384d1d
Matheus França <mfelippefranca>
2018-08-27 21:26:30 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/sin/progmulti/meuprimeiroapp/DisplayMessageActivity.java
@@ -0,0 +1,34 @@
+package br.usjt.sin.progmulti.meuprimeiroapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Nome: Mariana Suellen Pereira de Souza
+ * RA: 818145754
+ */
+public class DisplayMessageActivity extends Activity {
+
+    /**
+     * Nome: Mariana Suellen Pereira de Souza
+     * RA: 818145754
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String mensagem = intent.getStringExtra(MainActivity.MENSAGEM);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(mensagem);
+
+        ViewGroup layout = findViewById(R.id.display_message_activity_layout);
+        layout.addView(textView);
+        )
+    }
+}
